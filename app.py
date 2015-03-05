e080517d4ed603ce56b5e8290a2f1663245d5822
buchatskiy <buchatskiy2@yandex.ru>
2015-03-06 00:57:17 +0200

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
 from momentjs import momentjs
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['WHOOSH_BASE'] = os.path.join(basedir, 'search.db')
 app.config['CSRF_ENABLED'] = True
 app.secret_key = '12AvdfGFHDdfvSDHFF4!76#$%7'
 db = SQLAlchemy(app)
